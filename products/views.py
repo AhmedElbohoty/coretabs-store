faa8fc7b9b117e3e645436aee671d047e1f26270
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2020-09-22 20:10:06 +0200

    Add products app

---
+++ products/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.utils import timezone
+
+
+# Create your views here.
+
+
+def say_hi(request, name):
+    return render(request, 'say-hi.html', {'name': name})
+
+
+def show_time(request):
+    context = {'time': timezone.now()}
+    return render(request, "show-time.html", context)
