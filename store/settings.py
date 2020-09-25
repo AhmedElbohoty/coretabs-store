e527a8d3b8705351732c6f067bbde2995d3f4d5f
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2020-09-25 07:51:03 +0200

    Prepare project for deplying

--- store/settings.py
+++ store/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'n06-5md9&4y4k+m@wa(quif*y$y6k-7seni5qm1+r)h03qhzdi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', ]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
