c668814b823145659c693668792fa7f965d07243
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2020-09-29 22:26:52 +0200

    Add price field and created_at

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.utils import timezone
 
 # Create your models here.
 
@@ -7,6 +8,8 @@ from django.urls import reverse
 class Product(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=500, blank=True, null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=1)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
