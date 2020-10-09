5259b572762192e8420288496b46c25fa4755739
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2020-10-09 07:51:21 +0200

    Update email and password

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,14 +8,20 @@ User = get_user_model()
 
 
 class SignUpForm(UserCreationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-inp'}))
     first_name = forms.CharField(
-        max_length=30, required=False, help_text="Optional.")
+        max_length=30, required=False, help_text="Optional.", widget=forms.TextInput(attrs={'class': 'form-inp'}))
     last_name = forms.CharField(
-        max_length=30,  required=False, help_text="Optional.")
+        max_length=30,  required=False, help_text="Optional.", widget=forms.TextInput(attrs={'class': 'form-inp'}))
     email = forms.EmailField(
-        max_length=254, help_text="Required. Inform a valid email address")
+        max_length=254, help_text="Required. Inform a valid email address", widget=forms.EmailInput(attrs={'class': 'form-inp'}))
     address = forms.CharField(
-        max_length=254,  required=False, help_text="Optional.", widget=forms.Textarea)
+        max_length=254,  required=False, help_text="Optional.", widget=forms.Textarea(attrs={'class': 'form-textarea'}))
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-inp'}))
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-inp'}))
 
     class Meta:
         model = User
